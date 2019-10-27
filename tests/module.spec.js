6f566e9d5ce38d8e4ee4836b88bb49f626b4d937
Thomas Taylor <thomas.taylor@kineo.com>
2019-10-27 16:08:07 +0000

    Add test implementation

--- tests/module.spec.js
+++ tests/module.spec.js
@@ -1,11 +1,36 @@
+const { Hook } = require('adapt-authoring-core');
 const should = require('should');
+const ServerModule = require('../lib/module');
+const Router = require('../lib/router');
 
 describe('Server module', function() {
   before(function() {
-    // any set-up should go here
+    this.server = new ServerModule(global.ADAPT.app, { name: require('../package.json').name });
   });
-  describe('#functionName()', function() {
-    it('should check something', function() {
+  describe('#constructor()', function() {
+    it('should expose a `root` Router', function() {
+      this.server.root.should.be.an.instanceof(Router);
+    });
+    it('should expose an `api` Router', function() {
+      this.server.api.should.be.an.instanceof(Router);
+    });
+    it('should expose a hook to modify requests', function() {
+      this.server.requestHook.should.be.an.instanceof(Hook);
+    });
+    it('should expose Express.js App listen function', function() {
+      this.server.listen.should.be.a.Function();
+    });
+    it('should expose Express.js App static function', function() {
+      this.server.static.should.be.a.Function();
+    });
+  });
+  describe('#url()', function() {
+    it('should return the URL of the server', function() {
+      this.server.url.should.be.a.String();
+    });
+  });
+  describe('#boot()', function() {
+    it('should accept requests on the specified URL/port', function() {
       false.should.be.true();
     });
   });
